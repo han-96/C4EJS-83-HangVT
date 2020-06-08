4993e02070dcd2add8810b8f4d822b20b906797f
han-96 <hangvu.96@outlook.com>
2020-06-09 02:41:46 +0700

    Update Bài làm Homework 7

---
+++ Homework_Day_6/script.js
@@ -0,0 +1,5 @@
+let h1 = document.getElementById('heading-1');
+h1.innerHTML = "Hello, my name is HEADING LEVEL 1";
+let body = document.getElementById('nice-body');
+let h2= '<h2>This is heading level 2</h2>';
+body.innerHTML += h2;
